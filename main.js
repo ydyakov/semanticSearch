64c9a79d423aa20763eb77f9339ebfe9fb58a213
Yordan Dyakov <yordan.dyakov@mentormate.com>
2025-09-28 20:08:42 +0300

    update

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 ﻿document.addEventListener("DOMContentLoaded", () => {
-  const currentSlide = parseInt(document.body.dataset.slide);
+    const currentSlide = parseInt(document.body.dataset.slide);
 
-  //  + CTRL
-  document.addEventListener("keydown", (e) => {
-    if (e.ctrlKey && e.key === "ArrowRight") {
-      window.location.href = `slide${currentSlide + 1}.html`;
-    }
-    if (e.ctrlKey && e.key === "ArrowLeft") {
-      window.location.href = `slide${currentSlide - 1}.html`;
-    }
+    //  + CTRL
+    document.addEventListener("keydown", (e) => {
+        if (currentSlide == 1) {
+            return;
+        }
+        if (e.ctrlKey && e.key === "ArrowRight") {
+            window.location.href = `slide${currentSlide + 1}.html`;
+        }
+        if (e.ctrlKey && e.key === "ArrowLeft") {
+            window.location.href = `slide${currentSlide - 1}.html`;
+        }
 
-    // Ctrl + N -> 
-    if (e.ctrlKey && /^[0-9]$/.test(e.key)) {
-      const target = parseInt(e.key);
-      window.location.href = `slide${target}.html`;
-    }
-  });
+        // Ctrl + N -> 
+        if (e.ctrlKey && /^[0-9]$/.test(e.key)) {
+            const target = parseInt(e.key);
+            window.location.href = `slide${target}.html`;
+        }
+    });
 });
 
 
@@ -44,3 +47,10 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+function copyCode(id) {
+    const text = document.getElementById(id).innerText;
+    navigator.clipboard.writeText(text).then(() => {
+       
+    });
+}
